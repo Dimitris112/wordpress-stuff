833c101fd053e67085f5bd6d903ae1ea7de6cf9f
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-11-01 17:27:48 +0200

    Add labels

--- includes/class-wordpress-stuff.php
+++ includes/class-wordpress-stuff.php
@@ -17,10 +17,28 @@ class WordpressStuff
 
     //register post type
     public function registerPostType()
-    {
+    {   // generate via Codeium or other - tweaked
+        $labels = [
+            'name' => 'Wordpress Stuff',
+            'singular_name' => 'Wordpress Stuff',
+            'menu_name' => 'Wordpress Stuff',
+            'name_admin_bar' => 'Wordpress Stuff',
+            'add_new' => 'Add New Stuff',
+            'add_new_item' => 'Add New Stuff',
+            'edit_item' => 'Edit Stuff',
+            'new_item' => 'New Stuff',
+            'view_item' => 'View Stuff',
+            'all_items' => 'All Stuff',
+            'search_items' => 'Search Stuff',
+            'parent_item_colon' => '',
+            'not_found' => 'No Wordpress Stuff found',
+            'not_found_in_trash' => 'No Wordpress Stuff found in Trash',
+        ];
+
         // args
         $args = [
             'label' => 'Wordpress Stuff',
+            'labels' => $labels,
             'description' => 'Intro to Wordpress Plugins',
             'show_ui' => true, // show in admin dashboard
         ];
